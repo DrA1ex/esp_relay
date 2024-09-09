0f6859cf007508182b4e43730ec6c9fd530bc42e
Alexander <DrA1ex@users.noreply.github.com>
2024-09-09 15:50:47 +0500

    Wifi settings in constants.h

--- src/constants.h
+++ src/constants.h
@@ -2,6 +2,12 @@
 
 #include "sys_constants.h"
 
+#define WIFI_MODE                               (WIFI_STA_MODE)
+
+#define WIFI_CONNECTION_CHECK_INTERVAL          (5000u)                 // Interval (ms) between Wi-Fi connection check
+#define WIFI_MAX_CONNECTION_ATTEMPT_INTERVAL    (0u)                    // Max time (ms) to wait for Wi-Fi connection before switch to AP mode
+                                                                        // 0 - Newer switch to AP mode
+
 #define MDNS_NAME                               "esp_relay"
 
 #define RELAY_COUNT                             (1u)                    // Up to 4 relay
@@ -18,3 +24,5 @@
 #define RELAY_SWITCH_INTERVAL                   (3000ul)                // Minimum interval between relay switching
 
 #define TIME_ZONE                               (5.f)                   // GMT +5:00
+
+#define MQTT                                    (0u)
